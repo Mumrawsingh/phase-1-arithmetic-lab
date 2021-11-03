a9ef04311224bcca342be4efdf8a962cc6023bd5
Matthew Umrawsingh <matthew.umrawsingh@gmail.com>
2021-11-03 17:40:37 -0400

    commit tree 7557751a6852f84f0dc578dc561cc7e0e22903cb

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2;
+
+let random = 62;
+
+const num3 = 19
+const num4 = 5
+let mod = num3 % num4;
+
+let max = (5,10,15,20)
+    Math.max(5,10,15,20);
